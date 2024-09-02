a441838967acab26ca20c094a10772f6ed8d070e
ClementBarrier <clement.barrier@epitech.eu>
2024-09-02 15:20:50 +0200

    [UPD] react ts project

---
+++ front/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="App-header">
+        <p>
+          Yo lékip
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
