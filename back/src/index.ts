bba93bad37e1603146c01478ae3ccc59c5817548
Sonny-Fourmont <sonny.fourmont@epitech.eu>
2024-09-02 18:39:19 +0200

    [ADD] first index.ts version

---
+++ back/src/index.ts
@@ -0,0 +1,36 @@
+/*
+** EPITECH PROJECT, 2024
+** B-SVR-500-LYN-5-1-survivor-killian.cottrelle
+** File description:
+** index
+*/
+
+import 'dotenv/config';
+import express, {Express, Request, Response} from "express";
+const app: Express = express();
+const cors = require('cors');
+import bodyParser from 'body-parser';
+
+const HOST: string = `${process.env.NODE_HOST}`;
+const PORT: number = parseInt(`${process.env.NODE_PORT}`);
+
+// Features
+import {initDb} from './config/dbConfig';
+initDb();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use('/');
+
+// Routes
+app.get('/', (req: Request, res: Response) => {
+    console.log(`[${Date()}] : User connected;`);
+    res.send("");
+});
+
+// Listener
+console.log("\n------------------  API IS READY !  ------------------\n");
+app.listen(PORT, () => {
+    console.log (`listening at http://${HOST}:${PORT}`);
+});
