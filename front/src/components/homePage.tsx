d3269d42c26ccabf5bcf0cb3c8e7aa7344f1a641
ClementBarrier <clement.barrier@epitech.eu>
2024-09-02 17:56:20 +0200

    [ADD] start of home page

---
+++ front/src/components/homePage.tsx
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2024
+** B-SVR-500-LYN-5-1-survivor-killian.cottrelle
+** File description:
+** homePage
+*/
+
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home page!</h1>
+    </div>
+  );
+};
+
+export default Home;
