638f8a7d2c60ff2ad30242363cbcb032e1a79038
Sonny-Fourmont <sonny.fourmont@epitech.eu>
2024-09-03 13:47:35 +0200

    [ADD] First router implementation

---
+++ back/src/routes/router.ts
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2024
+** B-SVR-500-LYN-5-1-survivor-killian.cottrelle [WSL: Ubuntu]
+** File description:
+** router
+*/
+
+import express, {Request, Response, Router} from "express";
+const router: Router = express.Router();
+
+router.get('/test', (req: Request, res: Response) => {
+    res.send(`[${Date()}] : User connected;`);
+});
+
+export default router;
