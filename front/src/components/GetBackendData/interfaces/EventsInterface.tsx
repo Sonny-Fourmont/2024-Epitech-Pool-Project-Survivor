8528b2d77ed8455f7abc9c9741f2c72bd786f7a6
ClementBarrier <clement.barrier@epitech.eu>
2024-09-06 11:25:02 +0200

    [ADD] displays of backend data

---
+++ front/src/components/GetBackendData/interfaces/EventsInterface.tsx
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2024
+** B-SVR-500-LYN-5-1-survivor-killian.cottrelle
+** File description:
+** EventsInterface
+*/
+
+export interface EventsData {
+    id: number
+    name: string
+    date: string
+    duration: number
+    max_participants: number
+    location_x: string
+    location_y: string
+    type: string
+    employee_id: number
+    location_name: string
+}
--- front/src/components/dashboardPage/DashboardPage.tsx
+++ front/src/components/dashboardPage/DashboardPage.tsx
@@ -5,14 +5,49 @@
 ** homePage
 */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import NavBar from '../navbar/Navbar';
+
 import SimpleLineChart from '../charts/LineChart';
 import StackedBarChart from '../charts/StackedBarChart';
 import BasicPie from '../charts/PieChart';
+
+import { CustomerData } from '../GetBackendData/interfaces/CustomersInterface';
+import { EventsData } from '../GetBackendData/interfaces/EventsInterface';
+
+import { getCustomers, getEvents } from '../GetBackendData/GetBackendData';
 // import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 
 const Dashboard: React.FC = () => {
+
+  const [customersData, setCustomersData] = useState<CustomerData[] | undefined>([]);
+  const [eventData, setEventsData] = useState<EventsData[] | undefined>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+
+    const loadCustomersData = async () => {
+      try {
+        const result = await getCustomers();
+        setCustomersData(result);
+    } catch (error) {
+        setError("Failed to fetch data");
+      }
+    };
+
+    const loadEventsData = async () => {
+      try {
+        const result = await getEvents();
+        setEventsData(result);
+    } catch (error) {
+        setError("Failed to fetch data");
+      }
+    };
+
+    loadCustomersData();
+    loadEventsData();
+  }, []);
+
   return (
     <>
       <NavBar/>
@@ -47,7 +82,7 @@ const Dashboard: React.FC = () => {
               <tr className='card-values'>
                 <td className='card-values-first-group'>
                   <p className='card-values-title'>Customers</p>
-                  <p className='card-values-number'>932</p>
+                  <p className='card-values-number'>{customersData?.length}</p>
                   <p className='green'>+ 12.37%</p>
                 </td>
                 <td className='card-values-second-group'>
@@ -80,7 +115,7 @@ const Dashboard: React.FC = () => {
               <tr className='card-values'>
                 <td className='card-values-first-group'>
                   <p className='card-values-title'>Monthly</p>
-                  <p className='card-values-number'>83</p>
+                  <p className='card-values-number'>{eventData?.length}</p>
                   <p className='green'>+ 4.63%</p>
                 </td>
                 <td className='card-values-second-group'>
