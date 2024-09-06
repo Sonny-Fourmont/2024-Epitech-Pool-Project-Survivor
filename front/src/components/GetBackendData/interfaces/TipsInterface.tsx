88104c2265d58bf671a75a8a2e36784fa2e5b0ee
ClementBarrier <clement.barrier@epitech.eu>
2024-09-06 13:48:04 +0200

    [ADD] getTips from the backend

---
+++ front/src/components/GetBackendData/interfaces/TipsInterface.tsx
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2024
+** B-SVR-500-LYN-5-1-survivor-killian.cottrelle
+** File description:
+** TipsInterface
+*/
+
+export interface TipsData {
+    id: number
+    title: string
+    tip: string
+}
