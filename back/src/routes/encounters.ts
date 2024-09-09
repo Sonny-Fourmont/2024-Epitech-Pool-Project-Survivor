b18d83f978e60ba33c59966d92bf145950957a95
Askel <axel.averly@epitech.eu>
2024-09-09 12:43:29 +0200

    [FIX] problem database

--- back/src/routes/encounters.ts
+++ back/src/routes/encounters.ts
@@ -15,45 +15,89 @@ import { Category } from "../config/dbClass";
 router.use(express.json());
 
 router.get('/encounters', (req: Request, res: Response) => {
-    const options = {
+    axios.request({
         method: 'GET',
         url: 'https://soul-connection.fr/api/encounters',
         headers: {
             'X-Group-Authorization': process.env.API_KEY,
             'Authorization': `Bearer ${jwToken}`
-        }
-    };
-    axios.request(options)
+        },
+    })
     .then(response => {
-        client.addManyDocumentInCollection(Category.Encounters, response.data);
-        res.status(response.status).send(response.data)
+
+        response.data.forEach((element: any) => {
+            axios.request({
+                method: 'GET',
+                url: `https://soul-connection.fr/api/encounters/${element.id}`,
+                headers: {
+                    'X-Group-Authorization': process.env.API_KEY,
+                    'Authorization': `Bearer ${jwToken}`
+                }
+            })
+            .then(response => {
+                console.log(`[${Date()}] : Got encounter n°${element.id} from external API;`);
+                client.addDocumentInCollection(Category.Encounters, response.data);
+            })
+            .catch(error => {
+                console.log('\x1b[31m%s\x1b[0m', `[${Date()}] : An error occurred on encounter ${element.id};`);
+                console.log(error.response.data)
+            });
+        });
+        (async () => {
+            const data: any = await client.getData(
+                Category.Encounters,
+                {})
+                res.status(response.status).send(data);
+        })()
     })
     .catch(error => {
-        console.log(`[${Date()}] : An error occurred, please try again with correct information;\n${error}`);
-        res.send(`[${Date()}] : An error occurred, please try again with correct information;\n${error}`);
+        console.log('\x1b[31m%s\x1b[0m', `[${Date()}] : An error occurred;`);
+        console.log(error.response.data)
+        res.status(error.response.status).send(error.response.data);
     });
 });
 
 router.get('/encounters/:id', (req: Request, res: Response) => {
-    const options = {
+    try {
+        console.log(`[${Date()}] : Got encounter n°${req.params.id} from Database;`);
+        (async () => {
+            const data: any = await client.getData(
+                Category.Encounters,
+                {id: parseInt(req.params.id)})
+                res.status(200).send(data[0]);
+        })();
+    } catch(error) {
+        console.log('\x1b[31m%s\x1b[0m', `[${Date()}] : An error occurred;`);
+        console.log(error)
+        res.status(404).send(error);
+    }
+});
+
+router.get('/encounters/customer/:id', (req: Request, res: Response) => {
+    axios.request({
         method: 'GET',
-        url: `https://soul-connection.fr/api/encounters/${req.params.id}`,
+        url: `https://soul-connection.fr/api/customers/${req.params.id}/image`,
         headers: {
             'X-Group-Authorization': process.env.API_KEY,
             'Authorization': `Bearer ${jwToken}`
-        }
-    };
-    axios.request(options)
+        },
+    })
     .then(response => {
-        console.log(`[${Date()}] : Get encounters n°${req.params.id};`);
-        client.addDocumentInCollection(Category.Encounters, response.data);
-        res.status(response.status).send(response.data)
+        (async () => {
+            const data: any = await client.getData(
+                Category.Encounters,
+                {id: parseInt(req.params.id)})
+                console.log(`[${Date()}] : User connected as encouter n°${req.params.id} with his image;`);
+                res.status(200).send(data[0]);
+        })();
     })
     .catch(error => {
-        console.log(`[${Date()}] : An error occurred, please try again with correct information;\n${error}`);
-        res.send(`[${Date()}] : An error occurred, please try again with correct information;\n${error}`);
+        console.log('\x1b[31m%s\x1b[0m', `[${Date()}] : An error occurred;`);
+        console.log(error.response.data)
+        res.status(error.response.status).send(error.response.data);
     });
 });
+
 
 router.get('/encounters/customer/:id', (req: Request, res: Response) => {
     const options = {
